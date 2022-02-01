d919d7eea86b9283bb918c2641c03f0ceea75453
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-01 11:05:37 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nikhil Henry on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
