6aa4217f54c0d51d5a96e63f908e96387ad95882
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-04 00:02:16 +0530

    Create base matching logic

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,12 +7,31 @@
 
 import Foundation
 
-struct MemoryGame<CardContent>{
+struct MemoryGame<CardContent> where CardContent: Equatable{
   private (set) var cards: Array<Card>
   
-  mutating func choose(_ chosenCard:Card){
-    let chosenIndex = cards.firstIndex{card in card.id == chosenCard.id}
-    cards[chosenIndex!].isFaceUp.toggle()
+  private var indexOfTheOneAndOnlyFaceUpCard:Int?
+  
+  mutating func choose(_ card:Card){
+    if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+        !cards[chosenIndex].isFaceUp,
+        !cards[chosenIndex].isMatched
+    {
+      if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard{
+        if cards[chosenIndex].content  == cards[potentialMatchIndex].content {
+          cards[chosenIndex].isMatched = true
+          cards[potentialMatchIndex].isMatched = true
+        }
+        indexOfTheOneAndOnlyFaceUpCard = nil
+      }else{
+//      make all the cards face down
+        for index in cards.indices{
+          cards[index].isFaceUp = false
+        }
+        indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+      }
+      cards[chosenIndex].isFaceUp.toggle()
+    }
   }
   
   init(numberOfPairsOfCards:Int, createCardContent:(Int)->CardContent){
