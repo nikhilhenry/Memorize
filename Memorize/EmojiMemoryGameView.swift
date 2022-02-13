279b01bd5e46041909b99a09b34038a0253e218b
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-13 16:16:49 +0530

    refactor content view name

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Memorize
 //
 //  Created by Nikhil Henry on 01/02/22.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 
-struct ContentView: View {
+struct EmojiMemoryGameView: View {
   @ObservedObject var viewModel:EmojiMemoryGame
   
   var body: some View {
@@ -73,6 +73,6 @@ struct CardView:View{
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     let game = EmojiMemoryGame()
-    ContentView(viewModel: game)
+    EmojiMemoryGameView(viewModel: game)
   }
 }
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -12,7 +12,7 @@ struct MemorizeApp: App {
   private let game = EmojiMemoryGame()
     var body: some Scene {
         WindowGroup {
-          ContentView(viewModel: game)
+          EmojiMemoryGameView(viewModel: game)
         }
     }
 }
