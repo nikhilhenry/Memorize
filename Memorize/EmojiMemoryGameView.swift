1eadc6f762b35cae21de163b6410a6642c52d782
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-13 18:49:03 +0530

    complete lecture 6 final code

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -15,7 +15,7 @@ struct EmojiMemoryGameView: View {
     VStack{
       Text("Memorize!").font(.largeTitle).foregroundColor(.black)
       ScrollView{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]){
           ForEach(game.cards){ card in
             CardView(card: card)
               .aspectRatio(2/3, contentMode: .fit)
@@ -53,20 +53,33 @@ struct ThemeView: View{
 struct CardView:View{
   let card: EmojiMemoryGame.Card
   var body: some View{
-    ZStack{
-      let shape = RoundedRectangle(cornerRadius: 20)
-      if card.isFaceUp{
-        shape.fill().foregroundColor(.white)
-        shape.strokeBorder(lineWidth: 3)
-        Text(card.content).font(.largeTitle)
-      }else if card.isMatched{
-        shape.opacity(0)
-      }
-      else{
-        shape.fill()
+    GeometryReader{ geometry in
+      ZStack{
+        let shape = RoundedRectangle(cornerRadius:drawingConstants.cornerRadius)
+        if card.isFaceUp{
+          shape.fill().foregroundColor(.white)
+          shape.strokeBorder(lineWidth: drawingConstants.lineWidth)
+          Text(card.content).font(font(in: geometry.size))
+        }else if card.isMatched{
+          shape.opacity(0)
+        }
+        else{
+          shape.fill()
+        }
       }
     }
   }
+  
+  private func font(in size:CGSize) -> Font{
+    .system(size: min(size.width,size.height) * drawingConstants.fontScale)
+  }
+  
+  private struct drawingConstants{
+    static let cornerRadius: CGFloat = 20
+    static let lineWidth:CGFloat = 3
+    static let fontScale:CGFloat = 0.8
+  }
+  
 }
 
 
