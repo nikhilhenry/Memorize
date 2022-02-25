232d38159d4aec0a9da1caa75fb2d4cb1ba31319
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-25 20:16:06 +0530

    implement controller intents

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -38,6 +38,10 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
     }
   }
   
+  mutating func shuffle(){
+    cards.shuffle()
+  }
+  
   init(numberOfPairsOfCards:Int, createCardContent:(Int)->CardContent){
     cards = []
     // add numberOfPairsOfCards x 2 cards to cards array
