7caa76d8c9bedd9e3a5bc508667bd20ec3325e2f
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-03-22 16:24:10 +0530

    add theme persistence

--- Memorize/ThemeStore.swift
+++ Memorize/ThemeStore.swift
@@ -5,58 +5,51 @@
 //  Created by Nikhil Henry on 04/02/22.
 //
 
-import Foundation
+import SwiftUI
 
-struct ThemeStore<CardContent> where CardContent: Hashable{
+// a simple, persistent storage place for Themes
+
+struct Theme<CardContent>:Identifiable,Codable where CardContent: Codable{
+  var name:String
+  var emojiSet:[CardContent]
+  var numberOfPairsToShow:Int
+  var color:String
+  let id:Int
   
-  private (set) var themes: [Theme] = []
+  fileprivate init(name: String, emojiSet: [CardContent], numberOfPairsToShow:Int,color:String, id: Int) {
+    self.name = name
+    self.emojiSet = emojiSet
+    self.numberOfPairsToShow = numberOfPairsToShow
+    self.color = color
+    self.id = id
+  }
+}
+
+class ThemeStore{
   
-  mutating func addTheme(_ theme:Theme){
+  @Published var themes = [Theme<String>]()
+  
+  // MARK: - Persistence Handling
+  
+  private let userDefaultsKey = "ThemeStore"
+  
+  private func storeInUserDefaults(){
+    UserDefaults.standard.set(try? JSONEncoder().encode(themes), forKey: userDefaultsKey)
+  }
+  
+  private func restoreFromUserDefaults(){
+    if let jsonData = UserDefaults.standard.data(forKey: userDefaultsKey){
+      if let decodedThemes = try? JSONDecoder().decode([Theme<String>].self,from: jsonData){
+        themes = decodedThemes
+      }
+    }
+  }
+  
+  
+  // MARK: - Intents
+  
+  func addTheme(_ theme:Theme<String>){
     themes.append(theme)
   }
   
-  func getRandomTheme()->Theme{
-    themes.randomElement()!
-  }
-  
-  
-  struct Theme {
-    var name:String
-    var emojiSet:[CardContent]
-    var numberOfPairsToShow:Int
-    var color:String
-    
-    //  the init with numberOfPairsToShow specified
-    init(name:String,emojiSet:[CardContent],numberOfPairsToShow:Int,color:String) {
-      self.name = name
-      self.emojiSet = emojiSet
-      self.numberOfPairsToShow = numberOfPairsToShow
-      self.color = color
-    }
-    //  the init without numberOfPairsToShow specified (Extra-credit 2)
-    init(name:String,emojiSet:[CardContent],color:String) {
-      self.name = name
-      self.emojiSet = emojiSet
-      self.color = color
-      if Int.random(in: 0..<1) == 1{
-        numberOfPairsToShow = emojiSet.count
-      }else{
-        //  show a random number of cards (Extra-credit 2)
-        numberOfPairsToShow = Int.random(in: 1..<emojiSet.count)
-      }
-      
-    }
-    
-    func getThemeContents()->[CardContent]{
-      let rawContents = self.emojiSet
-      //    get unique list of array items
-      let processedContents = Set(rawContents)
-      return processedContents.shuffled()
-    }
-    
-    func getNumberOfPairsToShow()->Int{
-      let uniqueItems = Set(emojiSet)
-      return numberOfPairsToShow > uniqueItems.count ? uniqueItems.count : numberOfPairsToShow
-    }
-  }
 }
